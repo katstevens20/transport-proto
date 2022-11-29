cdbd93688e7f353e51e2421a5fc57274374138de
Khalid Atlassi <katlassi_ext@coyote-group.com>
2022-11-29 17:48:41 +0100

    first commit

---
+++ src/message/type/PBBool.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Kat\TransportProto\message\type;
+
+use Kat\TransportProto\message\PBMessage;
+
+/**
+ * @author Nikolai Kordulla
+ */
+class PBBool extends PBInt
+{
+        var $wired_type = PBMessage::WIRED_VARINT;
+
+        /**
+         * Parses the message for this type
+         *
+         * @param array
+         */
+        public function ParseFromArray()
+        {
+                $this->value = $this->reader->next();
+                $this->value = ($this->value != 0) ? 1 : 0;
+                
+                $this->clean();
+        }
+
+}
